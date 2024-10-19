534c7685f6ff208f932fe49165d335532eb41072
Fabo01 <fgarcia2024@alu.uct.cl>
2024-10-19 02:44:25 -0300

    Si lo subes asi a tu pillan deberia de funcionar :p borre todos los users de la bd

--- entrega2/pages/inicio.php
+++ entrega2/pages/inicio.php
@@ -25,16 +25,16 @@ if ($result->num_rows > 0) {
     if (password_verify($pass, $row['passwd'])) {
         $_SESSION['username'] = $username; // Guardar el usuario en la sesión
         echo "Inicio de sesión exitoso. Bienvenido, $username.";
-        header("Location: /~marcelo.vidal/DesWeb/Proyecto/entrega2/templates/home.html");
+        header("Location: ../templates/home.html");
         exit();
     } else {
         echo "Contraseña incorrecta.";
-        header("Location: /~marcelo.vidal/DesWeb/Proyecto/entrega2/index.html");
+        header("Location: ../index.html");
         exit();
     }
 } else {
     echo "Usuario no encontrado.";
-    header("Location: /~marcelo.vidal/DesWeb/Proyecto/entrega2/index.html");
+    header("Location: ../index.html");
     exit();
 }
 
